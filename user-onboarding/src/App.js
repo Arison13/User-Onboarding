7828ccffd23cbad680f53eb4c9f60d387d5a51c4
Arison13 <arisonarias@gmail.com>
2021-09-15 15:29:36 -0400

    got the basic form inputs setup

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Form name="Arison" value="Arias"/>
+      <Form/>
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,15 +1,41 @@
 import React from 'react'
 
 
-export default function Form (props) {
-    const {
-        name,
-        value,
-    }  = props
-    return (
-    <div>
-        <h1>ALIVE</h1>
-        <h2>{props.name} {props.value}</h2>
-     </div>
+export default function Form () {
+
+  return (
+    <form>  
+      <div> 
+        <label>Name
+            <input
+                name='Username'
+                type='text'
+            />
+        </label>
+        
+        <label>Email
+            <input
+                name='email'
+                type='text'
+            />
+        </label>
+        <label>Password
+            <input
+                name='password'
+                type='password'
+            />
+        </label>
+        <label>Terms of Service
+          <input
+            type="checkbox"
+            name="ToS"
+          />
+        </label>
+        <label>
+            <input type="submit" name="submit" />
+        </label>
+      </div>
+    </form>
+    
     )
 }
